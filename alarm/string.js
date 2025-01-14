bd881de09113ec444b322631e295f3becf81be30
raginigoswami19485 <raginigoswami19485@gmail.com>
2025-01-15 00:37:42 +0530

    project

--- alarm/string.js
+++ alarm/string.js
@@ -30,3 +30,14 @@ let rune=()=>{
     }
   },1000)
   }
+
+  VANTA.NET({
+    el: "#backg",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00
+  })
